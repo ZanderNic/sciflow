5d40e0cd7ed02026fe9f5fd6f47357e7f445e312
ZanderNic <nic.zander@thzander.de>
2026-04-27 17:43:53 +0200

    init commit :star:

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",   # testet on 3.10.12 and 3.9.20    
+    python_requires=">=3.9",
 )
